e9a4822ed471ce8d5354b7aa83549cd87649a99c
Facundo Tanovich <facundotanovich@gmail.com>
2022-04-29 18:35:28 -0300

    Fin seccion 24

---
+++ controllers/auth.js
@@ -0,0 +1,111 @@
+const bcrypt = require('bcryptjs/dist/bcrypt');
+const { response } = require('express');
+const { generarJWT } = require('../helpers/jwt');
+const Usuario = require('../models/Usuario');
+
+
+const crearUsuario = async(req, res = response) => {
+    const { email, password } = req.body;
+
+    try {
+        let usuario = await Usuario.findOne({ email });
+
+        if( usuario ) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El usuario ya existe'
+            });
+        }
+        
+        usuario = new Usuario(req.body)
+
+        //Encriptar contraseña
+        const salt = bcrypt.genSaltSync();
+        usuario.password = bcrypt.hashSync(password, salt);
+
+        await usuario.save();
+
+        //Gernerar el token
+        const token = await generarJWT(usuario.id, usuario.name);
+        
+        res.status(201).json({
+            ok: true,
+            uid: usuario.id,
+            name: usuario.name,
+            token,
+            msg: 'Usuario creado'
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Error inesperado'
+        })
+    }
+        
+ }
+
+const loginUsuario = async(req, res = response) => {
+    const { email, password } = req.body;
+
+    try {
+        let usuario = await Usuario.findOne({ email });
+
+        if( !usuario ) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Usuario y/o contraseña no existen'
+            });
+        }
+        //Confirmar los passwords
+        const validPassword = bcrypt.compareSync(password, usuario.password);
+
+        if(!validPassword){
+            return res.status(400).json({
+                ok: false,
+                msg: 'Usuario y/o contraseña no existen'
+            });
+        }
+
+        //Generar el token
+        const token = await generarJWT(usuario.id, usuario.name);
+
+        res.status(201).json({
+            ok: true,
+            uid: usuario.id,
+            name: usuario.name,
+            token
+        });
+
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Error inesperado'
+        });
+    }
+}
+
+const revalidarToken = async(req, res = response) => {
+
+    const { uid, name} = req;
+
+    const token = await generarJWT(uid, name);
+
+    res.json({
+        ok: true,
+        msg: 'Token renovado',
+        token
+    })
+}
+
+
+
+module.exports = {
+    crearUsuario,
+    loginUsuario,
+    revalidarToken
+}
---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { dbConnection } = require('./db/config');
+require('dotenv').config();
+const cors = require('cors');
+
+//Crear el server de express
+
+const app = express();
+
+// Base de Datos
+dbConnection();
+
+//Cors
+app.use(cors());
+
+//Directorio publico
+app.use(express.static('public'));
+
+//Lectura y parseo del body
+app.use(express.json());
+
+//Rutas
+app.use('/api/auth', require('./routes/auth'));
+//TODO: CRUD: Eventos
+
+//Escuchar peticiones
+app.listen( process.env.PORT, () => {
+    console.log( `El servidor esta corriendo en el puerto ${ process.env.PORT }` );
+} );
